c74001e0b369f7c0da696802f2428ffe026f8e1d
Yukie-Billal <yukiembillal01@gmail.com>
2024-11-30 07:53:25 +0700

    feat: tags repo

--- src/repository/tags.ts
+++ src/repository/tags.ts
@@ -2,3 +2,13 @@ import supabase from "../config/supabase"
 import {TablesInsert, TablesUpdate} from "../types/database.types"
 
 export const getTags = async () => await supabase.from("tags").select()
+
+export const createTags = async (values: TablesInsert<"tags">) => await supabase.from("tags").insert(values)
+
+export const updateTags = async (values: TablesUpdate<"tags">, id: number) => await supabase.from("tags").update(values).eq("id", id)
+
+export const deleteTags = async (id: number) => await supabase.from("tags").delete().eq("id", id)
+
+export const getMainTags = async () => await supabase.from("tags").select().filter("tag_id", "is", null)
+
+export const getTagsByMainTagId = async (tag_id: number) => await supabase.from("tags").select().eq("tag_id", tag_id)
